ebe4b3911db143d1a385bb958706a69e56d02589
chanpen0811 <jan1574p@gmail.com>
2023-10-24 15:37:58 +0700

    first commit

---
+++ compare.js
@@ -0,0 +1,19 @@
+//Compare operator
+const a = 2;
+const b = "2";
+
+//1. Equal Operator
+console.log(2 == 2); //true
+console.log(2 == '2'); //true
+
+//2. Not Equal Operator
+console.log(2 !=2);//false
+console.log('hello' != 'Hello');//true
+
+//3.Strc equal Operator
+console.log(2 === 2); //true
+console.log(2 === '2');//false
+
+//4.Strc not equal Operator
+console.log(2 !== '2'); //true
+console.log(2 !== 2);//false
